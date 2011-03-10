cf943900c50d8f7831f1298ce2f1b268abe36761
Dejapong <dejapong@desktopaero.com>
2011-03-09 18:54:24 -0800

    Added example tests

--- project/test/exampleTest.js
+++ project/test/exampleTest.js
@@ -1,12 +1,13 @@
 /** 
-	This test is intended to be run in node via the command line 
+	Example of a command line test for javascript for Dev. 
+	
+	This test can be run in node via the command line 
 	To run, use: 
 	
 	node exampleTest.js
 	
 	sys.puts("string") to display a string
 	sys.inspect(object) to return a string representation of an object
-	
 */
 
 var sys = require("sys"),
@@ -33,5 +34,65 @@ sys.puts("Aircraft X is: " + aircraft.getX());
 sys.puts("Aircraft Y is: " + aircraft.getY());
 sys.puts("Aircraft Alpha is: " + aircraft.getAlpha()); 
 
+/*
+	Fastest loop in javascript without Duff's device or reversed
+	iteration looks like this: 
+*/
+for (var i =0, il = 100; i < il; i++){
+	//loop body
+}
+
+/** To iterate through an object's members you can do this: */
+var testObject = {
+	name:"Deja",
+	age:"25",
+	interests:"Long walks on the beach",
+}
+
+for (member in testObject){ 
+	sys.puts(member + " : " + testObject[member]);
+}
+
+/** 
+	That's a lot slower than the first loop. Basically, only use this 
+	if you don't know what members exist in an object. 
+	
+	Also, you can use both dot and array notation on objects
+*/
+testObject.name == testObject["name"] ? sys.puts(green+"True"+white) : sys.puts(red+"False"+white);
+
+/** 
+	On the UI thread, if you want the interface to update between 
+	iterations, you can use setInterval(). This repeats the passed 
+	function at some interval in milliseconds. setTimeout() is related
+	but only calls the provided function once, after the timeout has 
+	elapsed. 
+	
+	This calls aircraft.tick every 100 milliseconds. Most browsers have 
+	resolution of 15ms, so this isn't very accurate. 
+*/
+var int1 = setInterval(aircraft.tick,100);
+
+/*
+	Stop intervals like this
+*/
+clearInterval(int1);
 
 
+/** Of course you inline the function like this: */
+var int2 = setInterval(function(){
+	//do whatever
+	aircraft.tick(); 
+	
+	/*
+		The keyword "this" refers to the current object. 
+		Here it refers to the calling interval. So we can kill this
+		interval after one tick like this: 
+	*/
+	clearInterval(this);
+	/*
+		really, if you wanted to do that though, you should use
+		setTimeout(); Usually, you would have some condition you 
+		watch for to clear the interval. 
+	*/
+},100);
