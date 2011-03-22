f5c754a1006f5b81d3639dcfa4a718fcf339a31c
Dejapong <dejapong@desktopaero.com>
2011-03-21 20:29:29 -0700

    orville changes

--- project/test/aircraftDynamics.js
+++ project/test/aircraftDynamics.js
@@ -1,13 +1,11 @@
 function AircraftDisplay(containerId, width, height){
-
 	var x0 = 400;
-	var y0 = 250;
-	
+	var y0 = 250;	
 	var paper = Raphael(containerId,width,height);	
 	var clouds = CloudsDisplay(paper,980,600);
 	var planePic = paper.image("../images/plane1.png",x0,y0,98*2,30*2);
 	var plane = spitfire;
-	plane.setState(100, 0, 0, 5, 5, 0, 0)
+	plane.setState(100, 0, 0, 5, 5, 0, 0);
 	plane.setElevatorAngle(0);
 	plane.setThrottle(0.5);
 	return {
